8cd7eead871113d4dc9b52659698e0b569ea4be4
Maria Victoria Gonzalez <mvg915@hotmail.com>
2024-02-18 23:51:34 -0300

    Trabajando con capas

--- entregable9/src/routes/sessions.router.js
+++ entregable9/src/routes/sessions.router.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import passport from "passport";
-import { authorization, passportCall, generateJWToken } from "../utils.js"
+import { authorization, passportCall, generateJWToken } from "../utils.js";
+import { githubLogin, loginUser, logoutUser, registerUser } from "../controllers/sessions.controller.js"
 
 const router = Router();
 
@@ -9,19 +10,7 @@ router.post(
   passport.authenticate("register", {
     failureRedirect: "api/sessions/fail-register",
   }),
-  async (req, res) => {
-    try {
-      console.log("Registrando usuario:");
-
-      res.status(201).send({
-        status: "success",
-        message: "Usuario creado con éxito con ID.",
-      });
-    } catch (error) {
-      console.error("Error al registrar usuario:", error);
-      res.status(500).send("Error interno del servidor");
-    }
-  }
+  registerUser
 );
 
 router.post(
@@ -29,52 +18,10 @@ router.post(
   passport.authenticate("login", {
     failureRedirect: "api/sessions/fail-login",
   }),
-  async (req, res) => {
-    try {
-      const user = req.user;
-      console.log(user);
-
-      const tokenUser = {
-        name: `${user.first_name} ${user.last_name}`,
-        email: user.email,
-        age: user.age,
-        role: user.role,
-      };
-      const access_token = generateJWToken(tokenUser);
-      console.log(access_token);
-
-      res.cookie("jwtCookieToken", access_token, {
-        maxAge: 60000,
-        httpOnly: true,
-      });
-      res.send({ message: "Login success!!" });
-    } catch (error) {
-      console.error("Error al procesar el inicio de sesión:", error);
-      res.status(500).send("Error interno del servidor");
-    }
-  }
+  loginUser
 );
 
-router.post("/logout", (req, res) => {
-  if (req.session.login || req.session.user) {
-    req.session.destroy((err) => {
-      if (err) {
-        console.error("Error al destruir la sesión:", err);
-        res
-          .status(500)
-          .json({ status: "error", message: "Error al cerrar sesión" });
-      } else {
-        res
-          .status(200)
-          .json({ status: "success", message: "Sesión cerrada exitosamente" });
-      }
-    });
-  } else {
-    res
-      .status(400)
-      .json({ status: "error", message: "No hay sesión activa para cerrar" });
-  }
-});
+router.post("/logout", logoutUser);
 
 router.get(
   "/github",
@@ -85,24 +32,7 @@ router.get(
 router.get(
   "/githubcallback",
   passport.authenticate("github", { failureRedirect: "/github/error" }),
-  async (req, res) => {
-    const user = req.user;
-
-    const tokenUser = {
-      name: `${user.first_name} ${user.last_name}`,
-      email: user.email,
-      age: user.age,
-      role: user.role,
-    };
-    const access_token = generateJWToken(tokenUser);
-    console.log(access_token);
-
-    res.cookie("jwtCookieToken", access_token, {
-      maxAge: 60000,
-      httpOnly: true,
-    });
-    res.redirect("/api/products");
-  }
+  githubLogin
 );
 
 router.get("/fail-register", (req, res) => {
